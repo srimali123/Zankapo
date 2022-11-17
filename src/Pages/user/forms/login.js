2e8a98b74654a272f5f753be995d6d9b6e470914
srimali12345 <110389813+srimali12345@users.noreply.github.com>
2022-11-18 01:33:56 +0530

    new login screen changes

--- src/Pages/user/forms/login.js
+++ src/Pages/user/forms/login.js
@@ -1,17 +1,44 @@
-import React from 'react'
-import { Checkbox, Button, Row, Col, Input } from "antd";
+import React from "react";
+import { Checkbox, Button, Row, Col, Input, Radio, } from "antd";
 import loginImg from "../../../assets/Images/common/bottomcover.png";
 import logo from "../../../assets/Images/common/LOGO.png";
-
+import logoBlue from "../../../assets/Images/common/bluelogo.png";
 export default function Login(props) {
-    
-
-    return (
-        
+  return (
     <div>
-      <Row className="loginPageConatiner" >
+      <Row className="loginPageConatiner">
+        <img src={logo} alt="new" className="logo" />
+
         <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-        <img src={logo} alt="new" className='logo' />
+          <div className="loginModalContainer">
+            <div className="loginModal">
+              <div className="loginModaTop">
+                <img src={logoBlue} alt="new" className="logoBue" />
+                <p className="loginHead">Log in to your account</p>
+                <p className="loginSubHead">
+                  Welcome back! Please enter your details.
+                </p>
+              </div>
+              <div className="loginModalContent">
+                <p className="labelLogin">Email</p>
+                <Input
+                  placeholder="Enter your email"
+                  className="loginInput emailInput"
+                />
+                <p className="labelLogin passwordText">Password</p>
+                <Input.Password className="loginInput " />
+
+                <div className="detailLogin">
+                <Radio className="radio">Remember for 30 days</Radio>
+               
+                <a href="#" className="forgetPsswordText">Forgot password</a>
+                </div>
+                <Button className="loginBtn">Login</Button>
+                <p className="haventAccText">Do not have an account? <a href="#" className="registerLink">Register here </a></p>
+              </div>
+            </div>
+          </div>
+
           <div
             className="leftSectionLogin"
             style={{
@@ -19,9 +46,7 @@ export default function Login(props) {
             }}
           ></div>
         </Col>
-     
       </Row>
     </div>
- 
-    )
+  );
 }
