2b9556afa80dbeab0ebe6e62babe91d15c209e56
srimali12345 <110389813+srimali12345@users.noreply.github.com>
2022-11-19 01:22:21 +0530

    login-changes-2

--- src/Pages/user/forms/login.js
+++ src/Pages/user/forms/login.js
@@ -11,7 +11,8 @@ export default function Login(props) {
 
         <Col xs={24} sm={24} md={24} lg={24} xl={24}>
           <div className="loginModalContainer">
-            <div className="loginModal">
+            {/* login modal */}
+            {/* <div className="loginModal">
               <div className="loginModaTop">
                 <img src={logoBlue} alt="new" className="logoBue" />
                 <p className="loginHead">Log in to your account</p>
@@ -36,7 +37,51 @@ export default function Login(props) {
                 <Button className="loginBtn">Login</Button>
                 <p className="haventAccText">Do not have an account? <a href="#" className="registerLink">Register here </a></p>
               </div>
+            </div> */}
+
+
+            {/* forget password */}
+            {/* <div className="loginModal forgetPasswordModal">
+              <div className="loginModaTop forgetmodalTop">
+                <img src={logoBlue} alt="new" className="logoBue" />
+                <p className="loginHead">Forgot password?</p>
+                <p className="loginSubHead forgetSubHead">
+                Enter your email and we will send you a link to reset your password
+                </p>
+              </div>
+              <div className="loginModalContent">
+                <p className="labelLogin">Email</p>
+                <Input
+                  placeholder="Enter your email"
+                  className="loginInput emailInput"
+                />
+               
+                <Button className="loginBtn">Continue</Button>
+               
+              </div>
+              <p className="footerText">2022 copyright Sankapo</p>
+            </div> */}
+
+            {/* create new passowrd */}
+            <div className="loginModal forgetPasswordModal newPasswordModal">
+              <div className="loginModaTop forgetmodalTop">
+                <img src={logoBlue} alt="new" className="logoBue" />
+                <p className="loginHead">Create a new password</p>
+                <p className="loginSubHead forgetSubHead">
+                Enter a new safe and secure password to use for your account
+                </p>
+              </div>
+              <div className="loginModalContent">
+              <p className="labelLogin passwordText">New password</p>
+                <Input.Password className="loginInput " />
+
+               
+                <Button className="loginBtn">Continue</Button>
+               
+              </div>
+              <p className="footerText newPasswordFooter">2022 copyright Sankapo</p>
             </div>
+
           </div>
 
           <div
