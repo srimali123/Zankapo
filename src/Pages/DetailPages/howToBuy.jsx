ec4a50cfc62e3ede0ff55e7d3fdecaacb3c196f7
Srimali Udayangani <srimali.ranawaka@1billiontech.com>
2023-04-13 09:27:54 +0000

    chnges

--- src/Pages/DetailPages/howToBuy.jsx
+++ src/Pages/DetailPages/howToBuy.jsx
@@ -7,13 +7,20 @@ function HowToBuy() {
       <h4>
       Sankapo.com is a Zambian website for buying and selling second-hand items. Here is a description of the process for buying an item on the site:
       </h4>
-      <h4>Browse the website and search for the item you are interested in purchasing. You can narrow your search using filters such as location, price range, and category.</h4>
-      <h4>Once you have found an item that you would like to buy, click on the listing to view the details and photos of the item</h4>
-      <h4>Read the description of the item and check the condition, price, and location. If you have any questions, you can contact the seller by clicking on the "Contact" button.</h4>
-      <h4>If you would like to proceed with the purchase, click on the "Buy" button. This will take you to the checkout page where you can review the details of the purchase and enter your payment information</h4>
-      <h4>Once you have entered your payment information and completed the checkout process, the seller will be notified of your purchase and will contact you to arrange the delivery or pickup of the item.</h4>
-      <h4> After the item has been delivered or picked up, the transaction is complete. It is recommended that you leave feedback for the seller to help other users make informed decisions when buying from them in the future</h4>
-  
+      <p>1.Browse the website and search for the item you are interested in purchasing. You can narrow your search using filters such as location, price range, and category.</p>
+      <p>Once you have found an item that you would like to buy, click on the listing to view the details and photos of the item</p>
+      <p>2.Read the description of the item and check the condition, price, and location. If you have any questions, you can contact the seller by clicking on the "Contact" button.</p>
+      <p>If you would like to proceed with the purchase, click on the "Buy" button. This will take you to the checkout page where you can review the details of the purchase and enter your payment information</p>
+      <p>Once you have entered your payment information and completed the checkout process, the seller will be notified of your purchase and will contact you to arrange the delivery or pickup of the item.</p>
+      <p>3.After the item has been delivered or picked up, the transaction is complete. It is recommended that you leave feedback for the seller to help other users make informed decisions when buying from them in the future</p>
+      <h3>Buyer Agreement:</h3>
+      <p>By using Sankapo.com to purchase items, you agree to the following terms and conditions:
+You acknowledge that Sankapo.com is not responsible for the quality, condition, or legality of any items sold on the site.</p>
+<p>You agree to communicate with the seller using the messaging feature on the site or through other means outside the platform, and to use a secure payment method when making a purchase.</p>
+<p>You understand that all transactions are between you and the seller, and that Sankapo.com is not responsible for any disputes or issues that may arise.</p>
+  <p>You agree to inspect the item carefully upon receipt and to contact the seller immediately if there are any discrepancies.</p>
+  <p>You agree to leave feedback for the seller after the transaction is complete to help other users make informed decisions when buying from them in the future.</p>
+  <p>By using Sankapo.com to purchase items, you agree to abide by these terms and conditions. Failure to do so may result in the suspension or termination of your account.</p>
     </div>
   )
 }
